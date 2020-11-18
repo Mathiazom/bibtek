aec705d45aee8f276d32b0a9db969d1525e2418f
Mathias Oterhals Myklebust <mathiom@stud.ntnu.no>
2020-11-18 21:25:24 +0100

    Changed terms page from own stage to scene

--- bibtek/fxui/src/main/java/bibtek/ui/TermsController.java
+++ bibtek/fxui/src/main/java/bibtek/ui/TermsController.java
@@ -1,8 +1,11 @@
 package bibtek.ui;
 
+import bibtek.ui.utils.ToastUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TermsController extends SceneChangerController {
 
@@ -13,9 +16,13 @@ public class TermsController extends SceneChangerController {
      * Closes the window.
      */
     public void close() {
-        Stage stage = (Stage) close.getScene().getWindow();
-        stage.close();
-
+        final Stage stage = (Stage) close.getScene().getWindow();
+        try {
+            this.changeScene(stage, "/bibtek/ui/CreateUser.fxml");
+        } catch (IOException e) {
+            ToastUtil.makeToast(stage, Toast.ToastState.ERROR, "There was an error showing the create user page");
+            e.printStackTrace();
+        }
     }
 
 }
